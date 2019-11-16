86bf66ebaf37fe30413c0d705b7de3a89bc868e6
KristinaLambang <kristinalambang@gmail.com>
2019-11-16 16:11:00 +0800

    Perbaikan Leger 16 Nov 2019

--- resources/views/masterdata/laporan/leger/index.blade.php
+++ resources/views/masterdata/laporan/leger/index.blade.php
@@ -2,68 +2,25 @@
 @section('content')
 <div class="section">
 	<div class="box box-primary">
-		<div class="box-header">
-		<div class="box-body">
-			<table id="example1" class="table table-bordered">
-				<thead>
-					<tr>  
-						<th rowspan="2"><p style="top: -50px">No</p></th>
-						<th rowspan="2"><p style="top: -50px">NIS</p></th>
-						<th rowspan="2"><p style="top: -50px">Nama Siswa</p></th>
-						<th rowspan="2"><p style="top: -50px">JK</p></th>
-						<th colspan="8">Nilai Pengetahuan</th>
-
-						<th colspan="8">Nilai Keterampilan</th>
-
-						<th rowspan="2"><p style="top: -50px">Jumlah</p></th>
-						<th rowspan="2"><p style="top: -50px">Jumlah Nilai</p></th>
-						<th rowspan="2"><p style="top: -50px">Ranking</p></th>
-					</tr>
-					<tr>
-						<td>Mtk</td>
-						<td>Bindo</td>
-						<td>Bing</td>
-						<td>Agm</td>
-						<td>Pkn</td>
-						<td>Sjrh</td>
-						<td>IPA</td>
-						<td>IPS</td>
-						<td>Mtk</td>
-						<td>Bindo</td>
-						<td>Bing</td>
-						<td>Agm</td>
-						<td>Pkn</td>
-						<td>Sjrh</td>
-						<td>IPA</td>
-						<td>IPS</td>
-					</tr>
-				</thead>
-				<tbody>
-				@foreach ($laporanleger as $row)
-					<tr>
-						<td>{{ $loop->iteration }}</td>
-						<td>{{ $row->nis }}</td>
-						<td>{{ $row->nama_siswa }}</td>
-						<td>{{ $row->jenis_kelamin }}</td>
-						<td>80</td>
-						<td>90</td>
-						<td>70</td>
-						<td>78</td>
-						<td>90</td>
-						<td>76</td>
-						<td>80</td>
-						<td>70</td>
-						<td>jumlah</td>
-						<td>jumlah_nilai</td>
-						<td>ranking</td>
-						<td class="box-footer">
-                			<a href="{{route('laporanleger.edit',$row->id_laporan_leger)}}" class="btn btn-success btn-xs"> Edit</a>
-        				</td>
-					</tr>
-				@endforeach
-				</tbody>
-			</table>
-			</div>
+			<h3>Leger</h3>
+	<div class="form-group">
+	
+	<div class="row">
+	<div class="col-sm-2">
+		<label>Tahun Ajaran</label>
+	</div>
+		<div class="col-sm-10">
+			<select name="tahun_ajaran" class="form-control" id="tahun_ajaran">
+				<option>2018/2019</option>
+				<option>2019/2020</option>
+			</select>
+        </div>
+    </div>
+	</div>
+	
+	<div class="box-header">
+			<p><a href="{{ route('absensi.create') }}" class="btn btn-success">Cetak</a></p>
+	</div>
             </div>
 		</div>
 	</div>
