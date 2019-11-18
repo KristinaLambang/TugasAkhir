bab96c8510ce979e556dcdd6213b79157e1fa421
KristinaLambang <kristinalambang@gmail.com>
2019-11-18 18:22:40 +0800

    Backup Database 18 Nov 2019

--- database4.sql
+++ database4.sql
@@ -29,6 +29,28 @@ CREATE TABLE `migrations` (
 
 /*Data for the table `migrations` */
 
+/*Table structure for table `tb_absensi` */
+
+DROP TABLE IF EXISTS `tb_absensi`;
+
+CREATE TABLE `tb_absensi` (
+  `id_absensi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_guru_walas` int(11) DEFAULT NULL,
+  `id_siswa` int(11) DEFAULT NULL,
+  `alpa` int(11) DEFAULT NULL,
+  `ijin` int(11) DEFAULT NULL,
+  `sakit` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_absensi`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_absensi` */
+
+insert  into `tb_absensi`(`id_absensi`,`id_guru_walas`,`id_siswa`,`alpa`,`ijin`,`sakit`,`created_at`,`updated_at`) values 
+(1,44,35,2,1,7,'2019-11-16 06:44:12','2019-11-16 06:59:50'),
+(3,44,31,1,2,0,'2019-11-16 07:41:24','2019-11-16 07:41:42');
+
 /*Table structure for table `tb_guru_kelas` */
 
 DROP TABLE IF EXISTS `tb_guru_kelas`;
@@ -87,7 +109,7 @@ CREATE TABLE `tb_jadwal` (
   `nama_pengajar` varchar(50) DEFAULT NULL,
   `nama_kelas` varchar(30) DEFAULT NULL,
   `nama_mapel` varchar(50) DEFAULT NULL,
-  `hari` varchar(10) DEFAULT NULL,
+  `hari` varchar(20) DEFAULT NULL,
   `dari_jam` time DEFAULT NULL,
   `sampai_jam` time DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
@@ -99,9 +121,9 @@ CREATE TABLE `tb_jadwal` (
 /*Data for the table `tb_jadwal` */
 
 insert  into `tb_jadwal`(`id_jadwal`,`nama_pengajar`,`nama_kelas`,`nama_mapel`,`hari`,`dari_jam`,`sampai_jam`,`created_at`,`updated_at`) values 
-(15,'39','17','20','Selasa','09:00:00','10:00:00','2019-11-14 11:23:02','2019-11-14 11:23:02'),
-(14,'39','17','21','Kamis','09:00:00','10:00:00','2019-11-10 11:44:45','2019-11-10 11:44:45'),
-(13,'38','16','20','Selasa','10:00:00','11:00:00','2019-11-10 11:43:00','2019-11-10 11:48:24');
+(15,'39','17','20','Senin','09:00:00','10:00:00','2019-11-14 11:23:02','2019-11-14 11:23:02'),
+(14,'39','17','21','Selasa','09:00:00','10:00:00','2019-11-10 11:44:45','2019-11-10 11:44:45'),
+(13,'38','16','20','Rabu','10:00:00','11:00:00','2019-11-10 11:43:00','2019-11-10 11:48:24');
 
 /*Table structure for table `tb_kelas` */
 
@@ -114,49 +136,25 @@ CREATE TABLE `tb_kelas` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id_kelas`),
   KEY `id_kelas` (`id_kelas`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_kelas` */
 
 insert  into `tb_kelas`(`id_kelas`,`nama_kelas`,`created_at`,`updated_at`) values 
+(19,'XI IPA PA','2019-11-16 08:52:55','2019-11-16 08:52:55'),
 (18,'X IPS PA','2019-11-09 13:25:06','2019-11-09 13:26:16'),
 (17,'X IPA PI','2019-11-09 13:24:55','2019-11-09 13:24:55'),
 (16,'X IPA PA','2019-11-09 13:24:46','2019-11-09 13:24:46');
-
-/*Table structure for table `tb_laporan_absensi` */
-
-DROP TABLE IF EXISTS `tb_laporan_absensi`;
-
-CREATE TABLE `tb_laporan_absensi` (
-  `id_laporan_absensi` int(11) NOT NULL,
-  `nis` int(20) DEFAULT NULL,
-  `nama_siswa` varchar(50) DEFAULT NULL,
-  `alpa` int(11) DEFAULT NULL,
-  `ijin` int(11) DEFAULT NULL,
-  `sakit` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id_laporan_absensi`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*Data for the table `tb_laporan_absensi` */
 
 /*Table structure for table `tb_laporan_leger` */
 
 DROP TABLE IF EXISTS `tb_laporan_leger`;
 
 CREATE TABLE `tb_laporan_leger` (
-  `id_laporan_leger` int(11) NOT NULL AUTO_INCREMENT,
-  `nis` int(11) DEFAULT NULL,
-  `nama_siswa` varchar(50) DEFAULT NULL,
-  `jenis_kelamin` varchar(30) DEFAULT NULL,
-  `nilai_pengetahuan` int(11) DEFAULT NULL,
-  `nilai_keterampilan` int(11) DEFAULT NULL,
-  `jumlah` int(11) DEFAULT NULL,
-  `jumlah_nilai` int(11) DEFAULT NULL,
-  `ranking` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_laporan_leger`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id_laporan_leger` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_laporan_leger` */
 
@@ -167,8 +165,8 @@ DROP TABLE IF EXISTS `tb_mapel`;
 CREATE TABLE `tb_mapel` (
   `id_mapel` int(11) NOT NULL AUTO_INCREMENT,
   `nama_mapel` varchar(50) DEFAULT NULL,
-  `id_kelas` varchar(20) DEFAULT NULL,
-  `id_guru_mapel` varchar(50) DEFAULT NULL,
+  `id_kelas` int(11) DEFAULT NULL,
+  `id_guru_mapel` int(11) DEFAULT NULL,
   `kkm` int(5) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
@@ -179,8 +177,8 @@ CREATE TABLE `tb_mapel` (
 /*Data for the table `tb_mapel` */
 
 insert  into `tb_mapel`(`id_mapel`,`nama_mapel`,`id_kelas`,`id_guru_mapel`,`kkm`,`created_at`,`updated_at`) values 
-(21,'Biologi','16','39',75,'2019-11-09 13:37:15','2019-11-09 13:37:29'),
-(20,'Bahasa Inggris','17','38',80,'2019-11-09 13:29:26','2019-11-09 13:36:42');
+(21,'Biologi',16,39,75,'2019-11-09 13:37:15','2019-11-09 13:37:29'),
+(20,'Bahasa Inggris',17,38,80,'2019-11-09 13:29:26','2019-11-09 13:36:42');
 
 /*Table structure for table `tb_nilai` */
 
@@ -199,10 +197,6 @@ CREATE TABLE `tb_nilai` (
   `ulangan_harian_2` double DEFAULT NULL,
   `uts` double DEFAULT NULL,
   `uas` double DEFAULT NULL,
-  `nilai_extrakurikuler` double DEFAULT NULL,
-  `nilai_sikap` double DEFAULT NULL,
-  `nilai_spiritual` double DEFAULT NULL,
-  `absensi` double DEFAULT NULL,
   `deskripsi_keterampilan` varchar(500) DEFAULT NULL,
   `deskripsi_pengetahuan` varchar(500) DEFAULT NULL,
   `nilai_raport` double DEFAULT NULL,
@@ -215,10 +209,53 @@ CREATE TABLE `tb_nilai` (
 
 /*Data for the table `tb_nilai` */
 
-insert  into `tb_nilai`(`id_nilai`,`id_guru_mapel`,`id_mapel`,`id_kelas`,`id_siswa`,`tahun_ajaran`,`tugas_1`,`tugas_2`,`ulangan_harian_1`,`ulangan_harian_2`,`uts`,`uas`,`nilai_extrakurikuler`,`nilai_sikap`,`nilai_spiritual`,`absensi`,`deskripsi_keterampilan`,`deskripsi_pengetahuan`,`nilai_raport`,`nilai_keterampilan`,`created_at`,`updated_at`) values 
-(22,39,20,17,31,'2019/2020',80,67,78,65,90,90,NULL,NULL,NULL,NULL,'Nice','Good',90,89,'2019-11-14 11:24:33','2019-11-14 11:24:33'),
-(21,39,21,17,31,'2019/2020',80,90,78,65,89,90,NULL,NULL,NULL,NULL,'Nice','Good',75,70,'2019-11-14 11:22:04','2019-11-14 11:22:04'),
-(20,39,21,17,35,'2019/2020',100,100,100,100,100,100,NULL,NULL,NULL,NULL,'Nice','Good',100,100,'2019-11-13 15:02:06','2019-11-13 15:02:34');
+insert  into `tb_nilai`(`id_nilai`,`id_guru_mapel`,`id_mapel`,`id_kelas`,`id_siswa`,`tahun_ajaran`,`tugas_1`,`tugas_2`,`ulangan_harian_1`,`ulangan_harian_2`,`uts`,`uas`,`deskripsi_keterampilan`,`deskripsi_pengetahuan`,`nilai_raport`,`nilai_keterampilan`,`created_at`,`updated_at`) values 
+(22,39,20,17,31,'2019/2020',80,67,78,65,90,90,'Nice','Good',90,89,'2019-11-14 11:24:33','2019-11-14 11:24:33'),
+(21,39,21,17,31,'2019/2020',80,90,78,65,89,90,'Nice','Good',75,70,'2019-11-14 11:22:04','2019-11-14 11:22:04'),
+(20,39,21,17,35,'2019/2020',100,100,100,100,100,100,'Nice','Good',100,100,'2019-11-13 15:02:06','2019-11-13 15:02:34');
+
+/*Table structure for table `tb_nilai_extrakurikuler` */
+
+DROP TABLE IF EXISTS `tb_nilai_extrakurikuler`;
+
+CREATE TABLE `tb_nilai_extrakurikuler` (
+  `id_nilai_extrakurikuler` int(11) NOT NULL AUTO_INCREMENT,
+  `id_guru_walas` int(11) DEFAULT NULL,
+  `id_siswa` int(11) DEFAULT NULL,
+  `nama_extrakurikuler` varchar(255) DEFAULT NULL,
+  `nilai_extrakurikuler` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_nilai_extrakurikuler`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_nilai_extrakurikuler` */
+
+insert  into `tb_nilai_extrakurikuler`(`id_nilai_extrakurikuler`,`id_guru_walas`,`id_siswa`,`nama_extrakurikuler`,`nilai_extrakurikuler`,`created_at`,`updated_at`) values 
+(1,44,31,'Renang',78,NULL,'2019-11-18 05:21:26'),
+(5,44,31,'Sepak Bola',78,'2019-11-16 07:36:50','2019-11-16 07:37:10');
+
+/*Table structure for table `tb_nilai_sikap` */
+
+DROP TABLE IF EXISTS `tb_nilai_sikap`;
+
+CREATE TABLE `tb_nilai_sikap` (
+  `id_nilai_sikap` int(11) NOT NULL AUTO_INCREMENT,
+  `id_guru_walas` int(11) DEFAULT NULL,
+  `id_siswa` int(11) DEFAULT NULL,
+  `nilai_sikap` int(11) DEFAULT NULL,
+  `nilai_spiritual` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_nilai_sikap`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_nilai_sikap` */
+
+insert  into `tb_nilai_sikap`(`id_nilai_sikap`,`id_guru_walas`,`id_siswa`,`nilai_sikap`,`nilai_spiritual`,`created_at`,`updated_at`) values 
+(1,44,35,78,90,NULL,'2019-11-16 07:31:33'),
+(6,44,31,89,78,'2019-11-16 05:14:33','2019-11-16 05:14:33'),
+(7,44,31,90,78,'2019-11-16 07:30:26','2019-11-16 07:30:26');
 
 /*Table structure for table `tb_siswa` */
 
@@ -250,22 +287,6 @@ CREATE TABLE `tb_siswa` (
 insert  into `tb_siswa`(`id_siswa`,`nis`,`nama_kelas`,`nama_siswa`,`jenis_kelamin`,`tempat_lahir`,`tanggal_lahir`,`agama`,`alamat`,`nama_ayah`,`pekerjaan`,`telepon`,`email`,`password`,`created_at`,`updated_at`) values 
 (35,9842078,'17','Komang','Laki-Laki','Denpasar','1996-02-01','Hindu','Gianyar','qwerty','mlklp','085337663493','komang@komang.com','komang','2019-11-09 13:53:55','2019-11-09 17:35:23'),
 (31,9842078,'16','Kristin','Perempuan','Denpasar','1998-02-13','Katolik','Jl Tukad Pakerisan No 40','Beni','Petani','085337663493',NULL,NULL,'2019-11-09 13:27:27','2019-11-09 13:27:27');
-
-/*Table structure for table `tb_tahun_ajaran` */
-
-DROP TABLE IF EXISTS `tb_tahun_ajaran`;
-
-CREATE TABLE `tb_tahun_ajaran` (
-  `id_tahun_ajaran` int(11) NOT NULL AUTO_INCREMENT,
-  `tahun_ajaran` varchar(50) DEFAULT NULL,
-  `semester` varchar(50) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id_tahun_ajaran`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-/*Data for the table `tb_tahun_ajaran` */
 
 /*Table structure for table `tb_walas` */
 
@@ -314,15 +335,15 @@ CREATE TABLE `users` (
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`level_user`,`created_at`,`updated_at`) values 
-(2,'Tata Usaha','admin@admin.com',NULL,'$2y$10$muWqzvpBgx84r2XGrcGi2O/P0SevA0rJw2S4xY5YvjVMCK/1rVl46','8oWoL5raSqbgOXsDaEwGkNB4Ix0BP9afjz8NSZXeWzm8IuRWD14tZG4ykCi6','admin','2019-04-08 03:55:04',NULL),
+(2,'Tata Usaha','admin@admin.com',NULL,'$2y$10$muWqzvpBgx84r2XGrcGi2O/P0SevA0rJw2S4xY5YvjVMCK/1rVl46','9TGYPvmTSkt1lUiRZkJWlKzk23tD58oOtVEmNragOukjSmFhG1fKW4iOruMX','admin','2019-04-08 03:55:04',NULL),
 (3,'Guru Mata Pelajaran','guru@guru.com',NULL,'$2y$10$i4qV.7z8nl.FZlBt6hCMiu5HZySUk9Hvqrn8I/IMmd2kDWntKiiJG','8RsbKhhjKDM0lAHUVe2tCgYD1Uyuj4GLsJlgKpCB5X1uUtqUrRbxRdfoge30','guru','2019-10-13 12:03:35','2019-10-13 12:03:35'),
-(5,'Guru Wali Kelas','walas@walas.com',NULL,'$2y$10$5dj3vL1O5I3Yvp3wmYMfvO1pvBd.5d20YcBXc4KM9.BkHbTbz4Fei','lFqBa4dOYuV5F4Hq9DqNoARubZh2iPyVbZJfK8IKPtFksaQU3moKFZDJGTLT','walas','2019-10-14 12:10:30','2019-10-14 12:10:30'),
-(6,'Siswa','siswa@siswa.com',NULL,'$2y$10$y0Z7QxyXHSvLRgp1eNA6EOtd.ycQyoL8.iqnljXklJIavMw6VIvMO','CPXry6AUZbnGHFBeL38uYq8194rFFdi9kvPvduQGddygE3g2ND4m41cCYs0A','siswa','2019-10-14 12:19:53','2019-10-14 12:19:53'),
+(5,'Guru Wali Kelas','walas@walas.com',NULL,'$2y$10$i4qV.7z8nl.FZlBt6hCMiu5HZySUk9Hvqrn8I/IMmd2kDWntKiiJG','pxpkIMQS4O30zXT3LHieXzs9rDx6zEQcCNiiUzJOJjxCFSvVz1utedfFPrHu','walas','2019-10-14 12:10:30','2019-10-14 12:10:30'),
+(6,'Siswa','siswa@siswa.com',NULL,'$2y$10$y0Z7QxyXHSvLRgp1eNA6EOtd.ycQyoL8.iqnljXklJIavMw6VIvMO','4lXJMZGGWDu9QPmeRPrQT9X7Uzs4fnK2YMyK03h5BJILdQSNW1ThyX2s7SCx','siswa','2019-10-14 12:19:53','2019-10-14 12:19:53'),
 (38,'Tuti Istiqomah, S.Pd.','tuti@tuti.com',NULL,'$2y$10$MyJmrL7D1hQ5YCt/g7ILKeC04FlUXj/0ouqy/Txi6F1Pms8RHQxWi',NULL,'guru','2019-11-09 17:16:21','2019-11-09 17:39:39'),
 (36,'Billy Iskandar, S.Pd','billy@billy.com',NULL,'$2y$10$Rr3SaTE.0V5Lb8KI5QQvjOR1t/rI7VqXliNyJ40vMF3VUnYVIpLua',NULL,'guru','2019-11-09 17:00:24','2019-11-09 17:00:24'),
-(39,'Puji Astuti, S.Pd','puji@puji.com',NULL,'$2y$10$WmD1SIEAoDANj8.bOZUmqutGpaPCP2o/CzlDy4nW9H37FvdzToa1O','gwhsfXeyFWonTlKDQQFWMWKteSCvtmmJo7fSKXxoaviFboqqPeuYrMxh2PTx','guru','2019-11-09 17:41:42','2019-11-09 17:47:36'),
+(39,'Puji Astuti, S.Pd','puji@puji.com',NULL,'$2y$10$WmD1SIEAoDANj8.bOZUmqutGpaPCP2o/CzlDy4nW9H37FvdzToa1O','v8X1mHH8IdCTYkoR2MTAQJqi5H4ZnduJgj9wc5ahRzQFPUxgcYTnbgDjCI1G','guru','2019-11-09 17:41:42','2019-11-09 17:47:36'),
 (35,'Komang','komang@komang.com',NULL,'$2y$10$Dnydp7PcpXIQ.Hge5Grn1OK5LjUnnkZF7irxUq3Lga76z6H1MNtaO',NULL,'siswa','2019-11-09 13:53:55','2019-11-09 17:35:24'),
-(44,'Dina Kirana, S.Pd','dina123@dina.com',NULL,'$2y$10$9.lcxkiD2L6ghb0PxX4MpuaaCfzjhYvacJqiCS0VN.BaAOtP5AvyC',NULL,'walas','2019-11-10 11:35:26','2019-11-10 11:39:23'),
+(44,'Dina Kirana, S.Pd','dina123@dina.com',NULL,'$2y$10$9.lcxkiD2L6ghb0PxX4MpuaaCfzjhYvacJqiCS0VN.BaAOtP5AvyC','y4aHeht7DH9NjoVdDg5DdvIdvKRWEhtSitm9pEAvSGmLMMf2KLqgXYZsbrW1','walas','2019-11-10 11:35:26','2019-11-10 11:39:23'),
 (31,'Kristin','kristin@kristin.com',NULL,'$2y$10$t56fkJXkRFuVdsUA/o2E0.17vMQlibfzykBVD9iEqLiJSpKmH7U8C',NULL,'siswa','2019-11-09 13:27:27','2019-11-09 13:27:27');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
